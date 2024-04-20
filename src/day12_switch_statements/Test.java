44ffad98d8f9798d663e2366580a390f712f3d72
MoseichukOleksandr <moseichuk.al@gmail.com>
2024-04-20 09:49:12 -0400

    String methods

---
+++ src/day12_switch_statements/Test.java
@@ -0,0 +1,19 @@
+package day12_switch_statements;
+
+public class Test {
+    public static void main(String[] args) {
+//        String opt = "true";
+//        switch (opt) {
+//            case "true":
+//                System.out.println("true");
+//                break;
+//            default:
+//                System.out.println("false");
+//        }
+//                System.out.println("Done");
+
+        float f1 = 14>5 ? 12456 : 12456.02f;
+        float f2 = f1 + 1024;
+        System.out.println(f2);
+    }
+}
